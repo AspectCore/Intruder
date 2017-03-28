0b834aeadbde74ceef9aedd7e800a4d7faab1e6f
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-03-28 22:55:24 +0800

    init project

---
+++ Publish.bat
@@ -0,0 +1,24 @@
+@echo off
+
+if not exist packages (
+    md packages
+)
+
+for /R "packages" %%s in (*) do (
+    del %%s
+)
+
+dotnet pack src/AspectCore.Extensions.Intruder --configuration Release --output packages
+
+for /R "packages" %%s in (*symbols.nupkg) do (
+    del %%s
+)
+
+set /p key=input key:
+set source=https://www.myget.org/F/aspectcore/api/v2/package
+
+for /R "packages" %%s in (*.nupkg) do ( 
+    call nuget push %%s -s %source% %key%
+)
+
+pause
